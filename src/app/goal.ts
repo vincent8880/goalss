7e86ca842018ff7a5e8fac9abf2cfd8024b9e4fb
root <ododovincent54@gmail.com>
2019-05-30 16:35:05 +0300

    Auto-generated commit

--- src/app/goal.ts
+++
@@ -1,8 +0,0 @@
-export class Goal {
-  public showDescription:boolean;
-  constructor(public id:number,
-     public name:string,public description:string,public completeDate:Date ){
-      this.showDescription=false
-
-  }
-}
